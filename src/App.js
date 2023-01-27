a9f0daea4830c02cb3582f87d48e04ba0ac3fa2d
edudias1972 <edudias1972@gmail.com>
2023-01-27 10:38:34 -0300

    Em desenvolvimentop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import RecipeInProgress from './components/Recipes/RecipeInProgress';
 import DoneRecipes from './pages/DoneRecipes/DoneRecipes';
 import Drinks from './pages/Drinks/Drinks';
 import DrinksDetails from './pages/Drinks/DrinksDetails';
-import DrinksInProgress from './pages/Drinks/DrinksInProgress';
 import FavoriteRecipes from './pages/FavoriteRecipes/FavoriteRecipes';
 import Login from './pages/Login/Login';
 import Meals from './pages/Meals/Meals';
 import MealsDetails from './pages/Meals/MealsDetails';
-import MealsInProgress from './pages/Meals/MealsInProgress';
 import Profile from './pages/Profile/Profile';
 
 function App() {
@@ -34,8 +33,8 @@ function App() {
       </Route>
       <Route path="/meals/:id" component={ MealsDetails } exact />
       <Route path="/drinks/:id" component={ DrinksDetails } exact />
-      <Route path="/meals/:id/in-progress" component={ MealsInProgress } />
-      <Route path="/drinks/:id/in-progress" component={ DrinksInProgress } />
+      <Route path="/meals/:id/in-progress" component={ RecipeInProgress } />
+      <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
     </Switch>
   );
 }
